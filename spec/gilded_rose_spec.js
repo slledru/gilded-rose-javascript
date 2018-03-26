45c2d3c3fb975c81d29256acb3f2aa648aee128c
slledru <slledru@msn.com>
2018-03-25 18:00:38 -0600

    refactored and all the test cases still pass

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -12,14 +12,6 @@ describe("Gilded Rose", function() {
       expect(items[idx].sell_in).toBe(testCase.sell_in);
     });
   })
-  /*
-  items.push(new Item('+5 Dexterity Vest', 10, 20));
-  items.push(new Item('Aged Brie', 2, 0));
-  items.push(new Item('Elixir of the Mongoose', 5, 7));
-  items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
-  items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
-  items.push(new Item('Conjured Mana Cake', 3, 6));
-  */
 
   it("After Day 1", function() {
     expected.push(new Item('+5 Dexterity Vest', 9, 19));
